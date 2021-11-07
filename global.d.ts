966a13cf71fc4ebc40176cf9468832dc06b5b3c3
John Camden <john.camden@gmail.com>
2021-11-07 17:26:40 -0500

    steps towards wdio

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+declare global {
+    namespace WebdriverIO {
+        interface Browser {
+            browserCustomCommand: (arg: any) => Promise<void>;
+        }
+
+        interface MultiRemoteBrowser {
+            browserCustomCommand: (arg: any) => Promise<void>;
+        }
+
+        interface Element {
+            elementCustomCommand: (arg: any) => Promise<number>;
+        }
+    }
+}
